303d2cfe0c9c302191c673b08003e1a8f1e01b39
Kara Cavanaugh <karacav@msn.com>
2020-10-25 18:47:02 -0400

    Solution 2 find largest elements

--- questions.java
+++ questions.java
@@ -1,4 +1,4 @@
-// fizz buzz
+// 1. fizz buzz
 import java.util.*; 
 public class Exercise116 {
  public static void main(String[] args)
@@ -15,3 +15,21 @@ public class Exercise116 {
         System.out.printf("\n");
     }
 }
+
+
+// 2. Write a program to find the k largest elements in a given array. Elements in the array can be in any order.
+import java.util.*;
+public class Solution {
+	
+    public static void main(String[] args) 
+    {
+        Integer arr[] = new Integer[]{1, 4, 17, 7, 25, 3, 100};
+        int k = 3;
+		System.out.println("Original Array: ");
+		System.out.println(Arrays.toString(arr));
+		System.out.println(k +" largest elements of the said array are:");
+        Arrays.sort(arr, Collections.reverseOrder());         
+       for (int i = 0; i < k; i++) 
+          System.out.print(arr[i] + " ");
+      }      
+  }
